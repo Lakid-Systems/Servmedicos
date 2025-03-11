83e7dd3e75de42150a1347c59e38be912bbfd899
Luis Marquez <luisivmaraz03@gmail.com>
2025-03-11 09:04:30 -0600

    Cambios en service 2 para imagenes

--- RM/src/components/Service2.vue
+++ RM/src/components/Service2.vue
@@ -1,8 +1,10 @@
 <script setup>
-import espacio from "../assets/images/espacio.svg";
-import area from "../assets/images/area.svg";
-import insumos from "../assets/images/insumos.svg";
+import { ref, onMounted } from "vue";
+import axios from "axios";
+
 const service1Heading = "Nuestros servicios";
+
+// Datos de los elementos del carrusel
 const items = [
   {
     id: 1,
@@ -20,132 +22,64 @@ const items = [
     des: "Mantén un inventario detallado de equipos médicos y materiales esenciales, evitando desabastecimientos y optimizando costos.",
   },
 ];
-const total = 3;
+
+const total = items.length;
+const images = ref({}); // Inicializado como objeto vacío
+
+onMounted(async () => {
+  try {
+    const response = await axios.get("http://localhost:3002/carrusel");
+    const data = response.data;
+
+    // Transformar el array en un objeto con id como clave
+    const imageMap = {};
+    data.forEach(item => {
+      imageMap[item.id] = item.image;
+    });
+
+    images.value = imageMap;
+    console.log("Imágenes cargadas:", images.value);
+  } catch (error) {
+    console.error("Error al cargar las imágenes:", error);
+  }
+});
 </script>
 
 <template>
-
   <div class="untree_co-section bg-light" id="services-section">
     <div class="container">
       <div class="row justify-content-between">
         <div class="col-lg-5 order-lg-2 js-custom-dots">
-          <span
-            class="caption"
-            data-aos="fade-up"
-            data-aos-delay="0"
-            :style="[{ color: themeColor }]"
-            >{{ service1Heading }}</span
-          >
-          <a
-            href="#"
-            class="service link horizontal d-flex active"
-            data-aos="fade-left"
-            data-aos-delay="0"
-          >
-            <div class="service-icon color-1 mb-4">
-              <svg
-                class="bi bi-app-indicator"
-                width="1em"
-                height="1em"
-                viewBox="0 0 16 16"
-                fill="currentColor"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M5.5 2A3.5 3.5 0 0 0 2 5.5v5A3.5 3.5 0 0 0 5.5 14h5a3.5 3.5 0 0 0 3.5-3.5V8a.5.5 0 0 1 1 0v2.5a4.5 4.5 0 0 1-4.5 4.5h-5A4.5 4.5 0 0 1 1 10.5v-5A4.5 4.5 0 0 1 5.5 1H8a.5.5 0 0 1 0 1H5.5z"
-                />
-                <path d="M16 3a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-              </svg>
-            </div>
+          <span class="caption" data-aos="fade-up" data-aos-delay="0">{{ service1Heading }}</span>
+          <a href="#" class="service link horizontal d-flex active" data-aos="fade-left" data-aos-delay="0">
             <div class="service-contents">
               <h3>{{ items[0].name }}</h3>
               <p>{{ items[0].des }}</p>
             </div>
           </a>
-          <a
-            href="#"
-            class="service link horizontal d-flex"
-            data-aos="fade-left"
-            data-aos-delay="100"
-          >
-            <div class="service-icon color-2 mb-4">
-              <svg
-                class="bi bi-arrow-repeat"
-                width="1em"
-                height="1em"
-                viewBox="0 0 16 16"
-                fill="currentColor"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M2.854 7.146a.5.5 0 0 0-.708 0l-2 2a.5.5 0 1 0 .708.708L2.5 8.207l1.646 1.647a.5.5 0 0 0 .708-.708l-2-2zm13-1a.5.5 0 0 0-.708 0L13.5 7.793l-1.646-1.647a.5.5 0 0 0-.708.708l2 2a.5.5 0 0 0 .708 0l2-2a.5.5 0 0 0 0-.708z"
-                />
-                <path
-                  fill-rule="evenodd"
-                  d="M8 3a4.995 4.995 0 0 0-4.192 2.273.5.5 0 0 1-.837-.546A6 6 0 0 1 14 8a.5.5 0 0 1-1.001 0 5 5 0 0 0-5-5zM2.5 7.5A.5.5 0 0 1 3 8a5 5 0 0 0 9.192 2.727.5.5 0 1 1 .837.546A6 6 0 0 1 2 8a.5.5 0 0 1 .501-.5z"
-                />
-              </svg>
-            </div>
+          <a href="#" class="service link horizontal d-flex" data-aos="fade-left" data-aos-delay="100">
             <div class="service-contents">
               <h3>{{ items[1].name }}</h3>
               <p>{{ items[1].des }}</p>
             </div>
           </a>
-          <a
-            href="#"
-            class="service link horizontal d-flex"
-            data-aos="fade-left"
-            data-aos-delay="200"
-          >
-            <div class="service-icon color-3 mb-4">
-              <svg
-                class="bi bi-briefcase"
-                width="1em"
-                height="1em"
-                viewBox="0 0 16 16"
-                fill="currentColor"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M0 12.5A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-6h-1v6a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-6H0v6z"
-                />
-                <path
-                  fill-rule="evenodd"
-                  d="M0 4.5A1.5 1.5 0 0 1 1.5 3h13A1.5 1.5 0 0 1 16 4.5v2.384l-7.614 2.03a1.5 1.5 0 0 1-.772 0L0 6.884V4.5zM1.5 4a.5.5 0 0 0-.5.5v1.616l6.871 1.832a.5.5 0 0 0 .258 0L15 6.116V4.5a.5.5 0 0 0-.5-.5h-13zM5 2.5A1.5 1.5 0 0 1 6.5 1h3A1.5 1.5 0 0 1 11 2.5V3h-1v-.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5V3H5v-.5z"
-                />
-              </svg>
-            </div>
+          <a href="#" class="service link horizontal d-flex" data-aos="fade-left" data-aos-delay="200">
             <div class="service-contents">
               <h3>{{ items[2].name }}</h3>
               <p>{{ items[2].des }}</p>
             </div>
           </a>
-          <a
-            href="#"
-            class="service link horizontal d-flex"
-            data-aos="fade-left"
-            data-aos-delay="300"
-          >
-            
-          </a>
         </div>
         <div class="col-lg-7">
           <div class="img-shadow">
             <div class="owl-single no-dots owl-carousel">
-              <div class="item">
-                <span class="number">{{ items[0].id }}/{{ total }}</span>
-                <img :src="espacio" alt="Image" class="img-fluid" />
-              </div>
-              <div class="item">
-                <span class="number">{{ items[1].id }}/{{ total }}</span>
-                <img :src="area" alt="Image" class="img-fluid" />
-              </div>
-              <div class="item">
-                <span class="number">{{ items[2].id }}/{{ total }}</span>
-                <img :src="insumos" alt="Image" class="img-fluid" />
+              <div class="item" v-for="item in items" :key="item.id">
+                <span class="number">{{ item.id }}/{{ total }}</span>
+                <img 
+                  :src="images[item.id] || 'https://via.placeholder.com/300'" 
+                  alt="Imagen de {{ item.name }}" 
+                  class="img-fluid"
+                />
               </div>
             </div>
           </div>
